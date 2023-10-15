3aa6a26c06e9178d382b29c929835f2a98e181c0
tshx0219 <143704677+tshx0219@users.noreply.github.com>
2023-10-15 10:32:23 +0800

    Update pipelineCreator.groovy

--- pipelineCreator.groovy
+++ pipelineCreator.groovy
@@ -7,7 +7,7 @@ multibranchPipelineJob("${Name}") {
                     repoOwner("devops-advanced-camp")
                     configuredByUrl(false)
                     repository("vote")
-                    repositoryUrl("https://github.com/devops-advanced-camp/vote.git")
+                    repositoryUrl("https://github.com/tshx0219/vote-app0.git")
                     credentialsId('github-pull-secret')
 
                     traits {
